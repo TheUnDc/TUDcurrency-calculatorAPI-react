2eee248bec4ef233cec31b1a7de113514169f1f6
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-08-20 23:10:17 +0200

    exported Select to module

---
+++ src/Select/index.js
@@ -0,0 +1,27 @@
+import "./style.css";
+
+const Select = ({name, selectedOne, selectedTwo}) => (
+    <select className="select__field" name={name}>
+        <option selected={selectedOne} value="EUR">Euro</option>
+        <option value="USD">Dolar</option>
+        <option selected={selectedTwo} value="PLN">Złoty</option>
+    </select>
+);
+
+export default Select;
+
+/*
+                      <select className="select__field" name="firstCurrency">
+                     <option selected value="EUR">Euro</option>
+                     <option value="USD">Dolar</option>
+                     <option value="PLN">Złoty</option>
+                  </select>
+*/
+
+/*
+                      <select className="select__field" name="secondCurrency">
+                     <option value="EUR">Euro</option>
+                     <option value="USD">Dolar</option>
+                     <option selected value="PLN">Złoty</option>
+                  </select>
+*/
