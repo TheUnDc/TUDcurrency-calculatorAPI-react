f8e348f86b8cc088733c0a2e5d4143e1f77ed554
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-08-20 23:10:52 +0200

    exported Legend and Label (FieldSetCaptions) to module

---
+++ src/FieldSetCaptions/index.js
@@ -0,0 +1,42 @@
+import "./style.css";
+
+const Label = ({innerText}) => (
+    <label>
+        <span className="caption__label">
+            {innerText}
+        </span>
+    </label>
+);
+
+const Legend = ({innerText}) => (
+    <legend className="fieldset__legend">
+        {innerText}
+    </legend>
+);
+
+export {
+    Label,
+    Legend,
+};
+
+/*
+                      <label>
+                     <span className="fieldset__labelText">
+                        Waluta końcowa:
+                     </span>
+                  </label>
+*/
+
+/*
+                      <label>
+                     <span className="fieldset__labelText">
+                        Waluta początkowa:
+                     </span>
+                  </label>
+*/
+
+/*
+                   <legend className="fieldset__legend">
+                  Kalkulator walut
+               </legend>
+*/
