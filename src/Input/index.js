c793def373ef3240d27ca3918a0b5bc1d134bdb0
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-08-20 22:20:41 +0200

    addded input without App.js (will be added later)

---
+++ src/Input/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Input = (props) => (
+    <input className="input" type={props.type} min={props.min} name={props.name} 
+        value={props.value} step={props.step} autofocus={props.autofocus} readonly={props.readonly}/>
+); 
+
+export default Input
+
+// <input readonly type="text" />
+// <input  type="number" min="0" autofocus name="waluta" value="1" step="any" />
+
+//<input className="fieldset__field" type="number" min="0" autofocus name="waluta" value="1" step="any" />
+//<input readonly className="fieldset__field" type="text" />
