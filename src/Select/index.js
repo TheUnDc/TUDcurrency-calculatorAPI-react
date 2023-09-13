7282e9896c2e89e545a4ccd221144f17a605b2c8
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-09-13 13:12:11 +0200

    exported Select css to styled components

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,11 +1,11 @@
-import "./style.css";
+import { SelectWrappper } from "./styled";
 
 const Select = ({name, selectChange, selectedValue,}) => (
-    <select className="select__field" name={name} onChange={selectChange} value={selectedValue}>
+    <SelectWrappper name={name} onChange={selectChange} value={selectedValue}>
         <option value="EUR">Euro</option>
         <option value="USD">Dolar</option>
         <option value="PLN">Złoty</option>
-    </select>
+    </SelectWrappper>
 );
 
 export default Select;
---
+++ src/Select/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SelectWrappper = styled.select`
+    padding: 2px;
+    border: none;
+    background-color:#ffffffaa;
+`;
